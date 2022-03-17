4476b99c647428296d71acb73f69bf7ad14a6f7d
unknown <tamiloreolaniyi@gmail.com>
2022-03-17 19:15:38 +0100

    Banner States and useEffect

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from '../axios'
+import requests from '../request';
 
 function Banner() {
     const [movie,setMovie]= useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.length - 1)
+                ]
+                )
+        }
+        fetchData();
+    },[])
   return (
     <header>
         

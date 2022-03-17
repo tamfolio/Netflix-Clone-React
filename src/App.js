4678cbe3c0494ffc28dadd2a1b6d1bc3202785be
unknown <tamiloreolaniyi@gmail.com>
2022-03-17 18:39:09 +0100

    building rows

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import requests from './request';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Row title='NETFLIX ORIGINAL'  fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
+      <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
+      <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from '../axios'
+import './row.css'
 
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
@@ -24,7 +25,10 @@ function Row({ title, fetchUrl}) {
         <h2>{title}</h2>
         <div className="row-posters">
             {movies.map(movie => (
-                <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                <img 
+                key={movie.id}
+                className='row-poster'
+                src={`${base_url}${movie.poster_path}`} alt={movie.name} />
             ))}
         </div>
     </div>

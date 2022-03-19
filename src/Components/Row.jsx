2a8d0d6ca6bca96cf0499abde5f6022f5770501e
unknown <tamiloreolaniyi@gmail.com>
2022-03-19 10:15:36 +0100

    Trailer component

--- src/Components/Row.jsx
+++ src/Components/Row.jsx
@@ -2,19 +2,20 @@ import React, {useState, useEffect} from 'react'
 import axios from '../axios'
 import './row.css'
 import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
 
-const base_url = 'https://image.tmdb.org/t/p/original/'
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState('')
+    const [trailerUrl, setTrailerUrl] = useState("")
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl)
             setMovies(request.data.results)
-            return request
+            return request;
         }
         fetchData();
     },[fetchUrl]);
@@ -34,7 +35,8 @@ function Row({ title, fetchUrl, isLargeRow}) {
         } else {
             movieTrailer(movie?.name || "")
             .then((url) => {
-
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'))
             })
             .catch((error) => console.log(error))
         }
